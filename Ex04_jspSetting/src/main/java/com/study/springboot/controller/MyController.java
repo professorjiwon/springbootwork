a81de3497178f98b34b1258019194426a87acb5d
professorjiwon <professorjiwon@gmail.com>
2024-08-06 14:51:42 +0900

    08.06_4교시

---
+++ Ex04_jspSetting/src/main/java/com/study/springboot/controller/MyController.java
@@ -0,0 +1,19 @@
+package com.study.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController {
+	@RequestMapping("/")
+	public @ResponseBody String root() {
+		return "jsp로 실행";
+	}
+	
+	@RequestMapping("/test1")
+	public String test1() {
+		return "test1";
+		//   /WEB-INF/views/test1.jsp 찾아서 web 으로 보여줌
+	}
+}
