54c490521ce0dc0823c3586dc12a0aa55837056c
professorjiwon <professorjiwon@gmail.com>
2024-08-06 15:57:28 +0900

    08.06_5교시

--- Ex04_jspSetting/src/main/java/com/study/springboot/controller/MyController.java
+++ Ex04_jspSetting/src/main/java/com/study/springboot/controller/MyController.java
@@ -14,5 +14,16 @@ public class MyController {
 	@RequestMapping("/test1")
 	public String test1() {
 		return "test1";
+		// 호출시   /WEB-INF/views/test1.jsp
+	}
+	
+	@RequestMapping("/test2")
+	public String test2() {
+		return "sub/test2";
+		// 호출시   /WEB-INF/views/sub/test2.jsp
 	}
 }
+
+
+
+
