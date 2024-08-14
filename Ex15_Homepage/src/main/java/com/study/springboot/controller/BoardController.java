08ab7bd55b215bfa0ce5477bdcb82ac2a2f4d210
professorjiwon <professorjiwon@gmail.com>
2024-08-14 11:09:38 +0900

    08.14_2교시

---
+++ Ex15_Homepage/src/main/java/com/study/springboot/controller/BoardController.java
@@ -0,0 +1,13 @@
+package com.study.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+	
+	@GetMapping("/list")
+	public String list() {
+		return "board/list";
+	}
+}
--- Ex15_Homepage/src/main/java/com/study/springboot/controller/MemberController.java
+++ Ex15_Homepage/src/main/java/com/study/springboot/controller/MemberController.java
@@ -76,5 +76,4 @@ public class MemberController {
 			status.setComplete();
 		return "redirect:/";
 	}
-
 }
