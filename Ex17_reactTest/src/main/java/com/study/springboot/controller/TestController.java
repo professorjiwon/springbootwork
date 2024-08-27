bcc21e3423c54b7df958a635c6fe0f1f01d76a10
professorjiwon <professorjiwon@gmail.com>
2024-08-27 16:06:46 +0900

    08.27_5교시

--- Ex17_reactTest/src/main/java/com/study/springboot/controller/TestController.java
+++ Ex17_reactTest/src/main/java/com/study/springboot/controller/TestController.java
@@ -1,15 +1,38 @@
 package com.study.springboot.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.study.springboot.domain.Menu;
+import com.study.springboot.service.MenuService;
 
 @RestController
 @RequestMapping("/api")
 public class TestController {
 	
+	@Autowired
+	MenuService menuService;
+	
 	@GetMapping("/test")
 	public String hello() {
 		return "테스트 입니다";
 	}
+	
+	@GetMapping("/menuall")
+	public List<Menu> menuAllList() {
+		return menuService.menuAllList();
+	}
+	
+	@PostMapping("/addmenu")
+	public Menu addMenu(@RequestBody Menu m) {
+		return menuService.insertMenu(m);
+	}
+	
 }
--- Ex17_reactTest/src/main/testreact/src/App.js
+++ Ex17_reactTest/src/main/testreact/src/App.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 
 function App() {
   const [hello, setHello] = useState('');
+  const [menuList, setMenuList] = useState([]);
 
   useEffect(() => {
     axios.get('/api/test')
@@ -17,6 +18,37 @@ function App() {
   return (
     <div className="App">
       <h3>서버에서 들어오 값 : {hello}</h3>
+      <br/>
+      <hr/>
+      <br/>
+
+      {
+        menuList.map((menu) => {
+          return(
+            <div>{menu.name}</div>
+          )
+        })
+      }
+      <button onClick={()=> {
+        axios.get('/api/menuall')
+             .then(result => {
+                console.log(result);
+                setMenuList(result.data);
+             })
+             .catch(()=>{
+                console.log("실패");
+             })
+      }}>서버에서 메뉴가져오기</button>
+
+      <button onClick={() => {
+        axios.post('/api/addmenu',{restaurant:'꾸이한끼', name:'불갈비덮밥', price:12000, type:'KR', taste:'HOT'})
+            .then(result => {
+              console.log(result);
+            })
+            .catch(()=>{
+              console.log("실패");
+           })
+      }}>메뉴 데이터 보내기</button>
     </div>
   );
 }
